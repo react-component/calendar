b54e27975a9a61b089bcbc336f9caf31b1402d48
yiminghe <yiminghe@gmail.com>
2014-12-18 19:40:39 +0800

    use md example

--- examples/index.js
+++ examples/index.js
@@ -3,7 +3,6 @@
 
 /** @jsx React.DOM */
 
-
 var Calendar = require('../');
 var CalendarInput = require('./CalendarInput');
 var GregorianCalendar = require('gregorian-calendar');
@@ -17,8 +16,33 @@ function onSelect(value) {
   console.log('onSelect');
   console.log(formatter.format(value))
 }
+
+var Skin = React.createClass({
+  getInitialState: function () {
+    return {
+      skin: '/assets/bootstrap.css'
+    }
+  },
+
+  onClick: function () {
+    this.setState({
+      skin: this.state.skin.indexOf('bootstrap')!==-1 ? '/assets/dpl.css' : '/assets/bootstrap.css'
+    });
+  },
+
+  render: function () {
+    return <div>
+      <link href={this.state.skin} rel="stylesheet"/>
+      <div>
+        <button onClick={this.onClick}>change skin</button>
+      </div>
+    </div>;
+  }
+});
+
 React.render(
   <div>
+    <Skin/>
     <h2>calendar (en-us)</h2>
     <Calendar showWeekNumber="1" onSelect={onSelect}/>
     <h2>input (zh-cn)</h2>
