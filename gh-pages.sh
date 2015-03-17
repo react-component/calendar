a891365df49cb2e710c2d133be70c0f92ba54d3b
yiminghe <yiminghe@gmail.com>
2015-03-17 18:09:47 +0800

    clean webpack

--- gh-pages.sh
+++
@@ -1,9 +0,0 @@
-npm run less
-npm run example
-cd ../calendar-gh-pages
-rm -rf build/
-mkdir build
-cp -r ../calendar/build/ build
-git add --all
-git commit -am "update examples"
-git push origin gh-pages:gh-pages
