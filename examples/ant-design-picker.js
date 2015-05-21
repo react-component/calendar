c2d54251e1823ecfce2417ef427052afccbe922f
yiminghe <yiminghe@gmail.com>
2015-05-21 19:06:27 +0800

    optimize ant-design

--- examples/ant-design-picker.js
+++ examples/ant-design-picker.js
@@ -8,69 +8,6 @@ var DateTimeFormat = require('gregorian-calendar-format');
 var GregorianCalendar = require('gregorian-calendar');
 var CalendarLocale = require('../lib/locale/zh-cn');
 
-var style = `
-@font-face {
-  font-family: 'iconfont';
-  src: url('//at.alicdn.com/t/font_1429685559_5814753.eot');
-  /* IE9*/
-  src: url('//at.alicdn.com/t/font_1429685559_5814753.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */ url('//at.alicdn.com/t/font_1429685559_5814753.woff') format('woff'), /* chrome、firefox */ url('//at.alicdn.com/t/font_1429685559_5814753.ttf') format('truetype'), /* chrome、firefox、opera、Safari, Android, iOS 4.2+*/ url('//at.alicdn.com/t/font_1429685559_5814753.svg#iconfont') format('svg');
-  /* iOS 4.1- */
-}
-
-.datepicker-input {
-  box-sizing: border-box;
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-  height: 32px;
-  width: 100%;
-  position: relative;
-  display: inline-block;
-  width: 100%;
-  margin: 0 0;
-  padding: 7px 10px;
-  border-radius: 6px 6px;
-  border: 1px solid #d9d9d9;
-  background-color: #ffffff;
-  color: #666;
-  line-height: 1.5;
-  -webkit-transform: border 0.3s cubic-bezier(0.35, 0, 0.25, 1), background 0.3s cubic-bezier(0.35, 0, 0.25, 1), box-shadow 0.3s cubic-bezier(0.35, 0, 0.25, 1);
-  transform: border 0.3s cubic-bezier(0.35, 0, 0.25, 1), background 0.3s cubic-bezier(0.35, 0, 0.25, 1), box-shadow 0.3s cubic-bezier(0.35, 0, 0.25, 1);
-  font-family: inherit;
-  vertical-align: baseline;
-}
-
-.datepicker-input:focus {
-  border-color: #23c0fa;
-  box-shadow: 0 0 3px #23c0fa;
-}
-
-.datepicker-input:hover {
-  border-color: #23c0fa;
-}
-
-.date-picker-wrap {
-  box-sizing: border-box;
-  position: relative;
-  display: block;
-  line-height: 1.5;
-  margin-bottom: 22px;
-}
-
-.datepicker-icon {
-  position: absolute;
-  -webkit-user-select:none;
-}
-
-.datepicker-icon:after {
-  position: relative;
-  left: -22px;
-  font-family: "iconfont";
-  content: "";
-  font-size: 12px;
-  color: #999;
-  top: 3px;
-  margin-right: -22px;
-}
-`;
 
 var Test = React.createClass({
   handleChange: function (value) {
@@ -114,19 +51,23 @@ var Test = React.createClass({
       orient={['top', 'left']}
       showTime={this.state.showTime} onSelect={this.handleCalendarSelect} onClear={this.handleCalendarSelect.bind(this, null)} showClear={true}/>;
     return <div style={{width: 236, margin: 20}} data-time={this.state.time}>
-      <style dangerouslySetInnerHTML={{__html: style}}>
-      </style>
       <div>
         <span>
           <input type='checkbox' checked={this.state.showTime} onChange={this.handleShowTimeChange} />
           showTime</span>
       </div>
-      <div className="date-picker-wrap">
+      <div style={{
+        'boxSizing': 'border-box',
+        'position': 'relative',
+        'display': 'block',
+        'lineHeight': 1.5,
+        marginBottom: 22
+      }}>
         <DatePicker
-          trigger={<span className="datepicker-icon" />}
+          trigger={<span className="rc-calendar-picker-icon" />}
           formatter={this.props.formatter} calendar={calendar}
           value={state.value} onChange={this.handleChange}>
-          <input type="text" className="datepicker-input" style={{background: 'white', cursor: 'pointer'}}/>
+          <input className="rc-calendar-picker-input"/>
         </DatePicker>
       </div>
     </div>;
