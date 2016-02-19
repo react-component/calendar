a03e5c87e0c6cb39bc88e71dccf1b91c689191ad
plandem <plandem@gmail.com>
2016-02-20 00:29:58 +0300

    Added files via upload

---
+++ src/locale/ru-RU.js
@@ -0,0 +1,26 @@
+import ruRU from '../gregorian-calendar-format/ru_RU';
+
+export default {
+	today: 'Сегодня',
+	now: 'Сейчас',
+	ok: 'Ok',
+	clear: 'Очистить',
+	month: 'Месяц',
+	year: 'Год',
+	monthSelect: 'Выберите месяц',
+	yearSelect: 'Выберите год',
+	decadeSelect: 'Выберите десятилетие',
+	yearFormat: 'yyyy',
+	dateFormat: 'M/d/yyyy',
+	monthFormat: 'MMMM',
+	monthBeforeYear: true,
+	previousMonth: 'Предыдущий месяц (PageUp)',
+	nextMonth: 'Следующий месяц (PageDown)',
+	previousYear: 'Предыдущий год (Control + left)',
+	nextYear: 'Следующий год (Control + right)',
+	previousDecade: 'Предыдущее десятилетие',
+	nextDecade: 'Следущее десятилетие',
+	previousCentury: 'Предыдущий век',
+	nextCentury: 'Следующий век',
+	format: ruRU,
+};
