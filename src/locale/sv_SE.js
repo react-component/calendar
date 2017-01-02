cdfeb2895a9f82bfb8fea4a82e2ea74308dca239
Jesper We <jesper@journeyman.se>
2017-01-02 14:32:20 +0100

    Update sv_SE.js

--- src/locale/sv_SE.js
+++ src/locale/sv_SE.js
@@ -24,5 +24,5 @@ export default {
   previousDecade: 'Föreg årtionde',
   nextDecade: 'Nästa årtionde',
   previousCentury: 'Föreg århundrade',
-  nextCentury: 'Nästa århundrade'
+  nextCentury: 'Nästa århundrade',
 };
