52d418ea6bd8a99689dd1b9ca120e4490453558e
Filip Rec <frec@igabinet.pl>
2017-06-05 15:53:03 +0200

    Add moment-formatted day cell title string
    This changes the way the title strings for day cells are generated - from hardcoded format to moment formatting.
    
    Resolves: https://github.com/react-component/calendar/issues/222

--- src/util/index.js
+++ src/util/index.js
@@ -19,7 +19,7 @@ export function getTodayTime(value) {
 }
 
 export function getTitleString(value) {
-  return `${value.year()}-${value.month() + 1}-${value.date()}`;
+  return value.format('L');
 }
 
 export function getTodayTimeStr(value) {
