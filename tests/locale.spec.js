3ac8af5e968e56faafc44dfce2ae9b91e141cf32
NC <35300439+golangmy@users.noreply.github.com>
2018-03-14 20:55:38 +0630

    add_myanmar_lang

--- tests/locale.spec.js
+++ tests/locale.spec.js
@@ -24,6 +24,7 @@ const locales = {
   ja_JP: require('../src/locale/ja_JP'),
   ko_KR: require('../src/locale/ko_KR'),
   ku_IQ: require('../src/locale/ku_IQ'),
+  my_MM: require('../src/locale/my_MM'),
   nb_NO: require('../src/locale/nb_NO'),
   nl_BE: require('../src/locale/nl_BE'),
   nl_NL: require('../src/locale/nl_NL'),
