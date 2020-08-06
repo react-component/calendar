e8e302d3d07ecb5fd72e3e6970511f84da8f93b8
yoyo837 <yoyo837@hotmail.com>
2020-08-06 19:45:41 +0800

    update

--- src/RangeCalendar.js
+++ src/RangeCalendar.js
@@ -162,7 +162,11 @@ class RangeCalendar extends React.Component {
         syncTime(prevSelectedValue[0], value);
         nextSelectedValue = [value];
       } else if (this.compare(firstSelectedValue, value) <= 0) {
-        syncTime(prevSelectedValue[1], value);
+        if (prevSelectedValue.length === 2 && this.compare(prevSelectedValue[0], prevSelectedValue[1]) == 0) {
+          syncTime(prevSelectedValue[1], prevSelectedValue[0]);
+        } else {
+          syncTime(prevSelectedValue[1], value);
+        }
         nextSelectedValue = [firstSelectedValue, value];
       } else {
         syncTime(prevSelectedValue[0], value);
