3bf8521fe007374bfef5ebdcc653d69797b223f8
ZeXiangTeo <zexiang2010@1utar.my>
2020-08-18 11:21:55 +0800

    add Title to full calendar

--- examples/full-calendar.js
+++ examples/full-calendar.js
@@ -10,10 +10,10 @@ import Select from 'rc-select';
 
 import zhCN from 'rc-calendar/src/locale/zh_CN';
 import enUS from 'rc-calendar/src/locale/en_US';
-
 import moment from 'moment';
 import 'moment/locale/zh-cn';
 import 'moment/locale/en-gb';
+import setTitle from './setTitle.js';
 
 const format = 'YYYY-MM-DD';
 const cn = location.search.indexOf('cn') !== -1;
@@ -36,6 +36,10 @@ class Demo extends React.Component {
   state = {
     type: 'month',
   };
+
+  componentDidMount() {
+    setTitle('Full Calendar');
+  }
 
   onTypeChange = (type) => {
     this.setState({
